209e67e799e06db5c4a27ec3b6194731577c31b8
Kenn Stack <kibadikennedy@gmail.com>
2021-07-30 03:47:21 +0800

    added custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+       'sans': ['poppins medium', 'montserrat regular'],
+       'serif': [ 'poppins medium', 'montserrat medium'],
+    },
+    extend: {
+      colors: {
+        'curious-blue': {
+          DEFAULT: '#239FCD',
+          '50': '#DCF1F9',
+          '100': '#C7E9F5',
+          '200': '#9BD8EE',
+          '300': '#70C6E7',
+          '400': '#44B5DF',
+          '500': '#239FCD',
+          '600': '#1C7DA1',
+          '700': '#145B76',
+          '800': '#0D3A4A',
+          '900': '#05181F'
+        },
+      }
+    },
   },
   variants: {
     extend: {},
