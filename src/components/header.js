50c4ac943ccccfbc1cfbff376934865a7eba2c3e
Kenn Stack <kibadikennedy@gmail.com>
2021-08-03 00:38:08 +0800

    deleted comments

--- src/components/header.js
+++ src/components/header.js
@@ -9,25 +9,7 @@ import HeaderTest from './HeaderTest'
 const Header = () => {
   return (
     <div className="bg-white shadow-sm backdrop-filter backdrop-blur-lg pt-1 top-0 z-50">
-
       <HeaderTest/>
-      {/* <div className="flex flex-row justify-between shadow-sm backdrop-filter backdrop-blur-lg pt-2 sticky top-0 z-10">
-        <Link to="/" className="flex flex-col mx-auto ml-3 md:ml-5">
-          <StaticImage
-            src='../images/creafrika-btp.png'
-            width={120}
-            quality={95}
-            placeholder="blurred"
-            layout="fixed"
-            formats={["AUTO", "WEBP", "AVIF"]}
-            alt="WebContract Logo"
-          />
-        </Link>
-        <Navbar/>
-      </div> */}
-      {/* <div className="text-2xl font-bold visible md:invisible text-gray-600 mr-10 cursor-pointer" >
-          <RiMenu3Fill/>
-      </div> */}
     </div>
   )
 }
