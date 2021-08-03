2e0c33b1ec85c4ec65fc63c7e09164290d9736e3
Kenn Stack <kibadikennedy@gmail.com>
2021-08-03 20:13:20 +0800

    added contact social media links

--- src/components/Contact/Main.js
+++ src/components/Contact/Main.js
@@ -2,6 +2,18 @@ import React from 'react'
 import { IoDownload } from 'react-icons/io'
 import Form from './Form'
 
+import { 
+    IoLogoLinkedin,
+    IoMailUnreadOutline
+} from 'react-icons/io5'
+
+import { ImBehance2 } from 'react-icons/im'
+import { 
+    FaWhatsappSquare, 
+    FaPhoneSquare,
+    FaInstagram
+} from 'react-icons/fa'
+
 const Main = () => {
     return (
         <div>
@@ -15,7 +27,53 @@ const Main = () => {
                         Nous sommes disponibles pour vous apporter notre expertise 💬
                     </p>
                 </div>
-                <Form/>
+                <div className="">
+                    <Form/>
+                    <div>
+                        {/* Logos Social Media */}
+                        <p className="text-md md:text-xl font-semibold text-center my-10 mx-5 md:mx-auto">
+                            Retrouvez-nous 💬
+                        </p>
+                        <div className="flex flex-row justify-evenly md:justify-evenly my-7 font-semibold p-5 shadow rounded md:mx-36">
+                                <a href="https://www.linkedin.com/in/komi-cr%C3%A9do-lokossou-3192591b3"  target="__blank" className="flex flex-row justify-center text-gray-600">
+                                    <div className="text-2xl lg:text-3xl">
+                                        <IoLogoLinkedin />
+                                    </div>
+                                    <h1 className="text-xs mt-1 mx-1 font-sans lg:text-sm">LinkedIn</h1>
+                                </a>
+                                <a href="mailto:creafrikagroup@gmail.com" target="__blank" className="flex flex-row justify-center text-gray-600 my-5">
+                                    <div className="text-2xl lg:text-3xl">
+                                        <IoMailUnreadOutline/>
+                                    </div>
+                                    <h1 className="text-xs mt-1 mx-1 font-sans lg:text-sm">E-mail</h1>
+                                </a>
+                                <a href="https://www.behance.net/credolokossou" target="__blank" className="flex flex-row justify-center text-gray-600 ">
+                                    <div className="text-2xl lg:text-3xl">
+                                        <ImBehance2/>
+                                    </div>
+                                    <h1 className="text-xs mt-1 mx-1 font-sans lg:text-sm">Behance</h1>
+                                </a>
+                                <a href="https://www.instagram.com/invites/contact/?i=58i02ctef785&utm_content=mj40uol" target="__blank" className="flex flex-row justify-center text-gray-600 my-5">
+                                    <div className="text-2xl lg:text-3xl">
+                                        <FaInstagram/>
+                                    </div>
+                                    <h1 className="text-xs mt-1 mx-1 font-sans lg:text-sm">Instagram</h1>
+                                </a>
+                                <a href="https://wa.me/8613681259863" target="__blank" className="flex flex-row justify-center text-gray-600 my-5">
+                                    <div className="text-2xl lg:text-3xl">
+                                        <FaWhatsappSquare/>
+                                    </div>
+                                    <h1 className="text-xs mt-1 mx-1 font-sans lg:text-sm">WhatsApp 1</h1>
+                                </a>
+                                <a href="https://wa.me/22899337961" target="__blank" className="flex flex-row justify-center text-gray-600 my-5">
+                                    <div className="text-2xl lg:text-3xl">
+                                        <FaWhatsappSquare/>
+                                    </div>
+                                    <h1 className="text-xs mt-1 mx-1 font-sans lg:text-sm">WhatsApp 2</h1>
+                                </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
