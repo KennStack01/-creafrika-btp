9951212a1be2e30d4b0bc2fce40766e71555818e
Kenn Stack <kibadikennedy@gmail.com>
2021-08-03 18:17:30 +0800

    added contact form

--- src/components/Contact/Main.js
+++ src/components/Contact/Main.js
@@ -1,24 +1,21 @@
 import React from 'react'
 import { IoDownload } from 'react-icons/io'
+import Form from './Form'
 
 const Main = () => {
     return (
         <div>
             <h1 className="text-4xl md:text-5xl font-semibold mx-2 text-gray-600">Vous satisfaire est notre Priorité Première</h1>
-            <p className="text-justify md:text-xl md:text-center mx-5 my-8 md:mx-40">
-                Le changement peut ne pas apporter de croissance 
-                mais il n'y a pas de croissance sans changement : 
-                La croissance d’une civilisation dépend aussi 
-                du changement architectural apportée dans cette communauté.
-            </p>
-
-            <div className="transform transition duration-500 hover:scale-110 my-14">
-                <div className="flex flex-row mx-auto w-72 p-2 bg-curious-blue-500 font-medium py-4 md:py-2 text-lg hover:bg-curious-blue-600 text-white my-14">
-                    Rencontrer notre équipe
-                    <div className="text-white text-3xl" >
-                        <IoDownload/>
-                    </div>
+            <div>
+                <div>
+                    <h1 className="text-2xl md:text-4xl font-semibold text-center my-4 md:my-8"> 
+                        🤝 <span className="bg-curious-blue-500">Travaillons</span> Ensemble!
+                    </h1>
+                    <p className="text-md md:text-xl font-semibold text-center my-10 mx-5 md:mx-auto">
+                        Nous sommes disponible pour vous apporter notre expertise 💬
+                    </p>
                 </div>
+                <Form/>
             </div>
         </div>
     )
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Main from '../components/Contact/Main'
 
 const Contact = () => {
     return (
         <Layout>
             <Seo title="Contact" description="Creafrica BTP, Nous contacter"/>
-
+            <Main/>
         </Layout>
     )
 }
