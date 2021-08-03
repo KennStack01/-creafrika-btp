5b3b0201240a87580a996abb3f507716d8025067
Kenn Stack <kibadikennedy@gmail.com>
2021-08-03 18:51:04 +0800

    finished endpoint for contact form

--- src/components/Contact/Form.js
+++ src/components/Contact/Form.js
@@ -8,7 +8,7 @@ const Form = () => {
             </h1>
             <div className="flex items-center w-full">
                 <div class="w-full bg-white rounded shadow p-8 m-4 md:max-w-sm md:mx-auto">
-                    <form class="mb-4 md:flex md:flex-wrap md:justify-between"  method="post" action={`https://getform.io/f/09c7f2c7-1e46-443f-baf0-bb394a33cb78`}>
+                    <form class="mb-4 md:flex md:flex-wrap md:justify-between"  method="post" action={`https://getform.io/f/8ddca5d3-daac-449c-97d2-3debd4ad0c8e`}>
                         <div class="flex flex-col mb-4 md:w-full">
                             <label className="mb-2 tracking-wide font-bold text-lg text-grey-darkest" for="first_name">Votre Nom Complet</label>
                             <input className="border py-2 px-3 text-grey-darkest focus:outline-none focus:ring-2 focus:ring-turbo-600 focus:ring-opacity-50" type="text" name="first_name" id="first_name" placeholder="Credo Lokossou" required/>
@@ -88,7 +88,7 @@ const Form = () => {
                             <textarea name="message" id="message"  className="resize-none p-2 h-36 border rounded focus:outline-none focus:ring-2 focus:ring-turbo-600 focus:ring-opacity-50" placeholder="Nous sommes ici pour vous accompagner dans vos projets, avec notre expertise." rows="4" required></textarea>
                         </div>
 
-                        <div className="mt-10 mx-auto">
+                        <div className="mt-10 mx-auto transform transition duration-500 hover:scale-110">
                             <button type="submit" className="text-white bg-curious-blue-600 font-bold py-4 md:py-2 px-6 text-xl hover:text-turbo-400">Envoyez🚀</button>
                         </div>
                     </form>
