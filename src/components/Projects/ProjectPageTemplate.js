78874b221e584bf2802f64d006d31e2ca0ad2094
Kenn Stack <kibadikennedy@gmail.com>
2021-08-05 04:22:58 +0800

    created dynamic content

--- src/components/Projects/ProjectPageTemplate.js
+++ src/components/Projects/ProjectPageTemplate.js
@@ -2,9 +2,39 @@ import React from 'react'
 import { graphql } from 'gatsby'
 
 const ProjectPageTeamplate = ({ data }) => {
+
+    const myProject = data.contentfulProject
+    const descriptionProjet = JSON.parse(myProject.descriptionDuProjet.internal.content)
+    let countKeyForPictures = 0
+
     return (
         <div>
-            <h1> Hello World! </h1>
+            <h1 className="text-2xl font-semibold"> {myProject.nomDuProjet} </h1>
+            <img 
+                src={myProject.imagePrincipale.file.url} 
+                alt="Le Projet" 
+                loading="lazy"
+                placeholder="blurred"
+                className=" mx-auto"
+            />
+            <div>
+                <h2> Année: {myProject.dateDuProjet} </h2>
+                <h2> Localisation: {myProject.localisation} </h2>
+            </div>
+            <p> {descriptionProjet.Description} </p>
+
+            <h1 className="mx-auto font-semibold text-xl"> En Images... </h1>
+            <div>
+                {myProject.multiplesImages.map((image) => (
+                    <img 
+                        src={image.file.url} 
+                        key={countKeyForPictures++} 
+                        alt="Le Projet" 
+                        loading="lazy" 
+                        className="" 
+                    />
+                ))}
+            </div>
         </div>
     )
 }
@@ -26,7 +56,7 @@ export const query = graphql`
                     content
                 }
             }
-            dateDuProjet
+            dateDuProjet(formatString: "YYYY", locale: "fr")
             multiplesImages {
                 file {
                     url
