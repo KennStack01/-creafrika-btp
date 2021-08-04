62110a8a7ea6b0b7cffeeb9fbe094047b7e7e1bd
Kenn Stack <kibadikennedy@gmail.com>
2021-08-05 00:55:05 +0800

    centered div

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby'
 const Project = ({picture, name, location, slug}) => {
     return (
         <Link to={`${slug}`} className="m-5">
-            <div className="w-72 pb-2 rounded shadow hover:shadow-lg">
+            <div className="w-80 md:w-72 mx-auto pb-2 rounded shadow hover:shadow-lg">
                 <img 
                     src={picture} 
                     alt="Le Projet" 
@@ -12,9 +12,9 @@ const Project = ({picture, name, location, slug}) => {
                     placeholder="blurred"
                     className=""
                 />
-                <div className="mx-auto mt-4 mb-10">
+                <div className="mx-auto mt-4 mb-10 text-gray-800">
                     <h1 className="font-semibold"> {name} </h1>
-                    <p className="font-medium"> {location} </p>
+                    <p className="text-xs font-medium text-gray-700"> {location} </p>
                 </div>
                 <div className="my-3">
                     <Link to={`${slug}`} className="px-3 py-2 rounded bg-curious-blue-500 hover:bg-curious-blue-600 text-white font-medium mx-auto">
